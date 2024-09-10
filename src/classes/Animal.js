e2d6b3467f0d215b8d5c2025c231293864b75b52
Luan Evander <luanevander.dev@gmail.com>
2024-09-10 12:10:26 -0300

    feat(Animal): adiciona construtor à classe Animal

--- src/classes/Animal.js
+++ src/classes/Animal.js
@@ -1,3 +1,8 @@
 class Animal {
-    
+    constructor(especie, tamanho, biomas, carnivoro = false) {
+        this.especie = especie;
+        this.tamanho = tamanho;
+        this.biomas = biomas;
+        this.carnivoro = carnivoro;
+    }
 }
