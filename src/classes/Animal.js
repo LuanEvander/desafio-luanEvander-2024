63fd8d605f880add8f03c4a16eccd3a43fbd7a2c
Luan Evander <luanevander.dev@gmail.com>
2024-09-12 11:26:40 -0300

    feat(Animal): refatora a classe Animal para adicionar construtor com base na espécie

--- src/classes/Animal.js
+++ src/classes/Animal.js
@@ -1,9 +1,48 @@
 class Animal {
-    constructor(especie, tamanho, biomas, carnivoro = false) {
-        this.especie = especie;
-        this.tamanho = tamanho;
-        this.biomas = biomas;
-        this.carnivoro = carnivoro;
+    constructor(especie) {
+        switch (especie) {
+            case 'LEAO':
+                this.especie = 'LEAO';
+                this.tamanho = 3;
+                this.biomas = ['savana'];
+                this.carnivoro = true;
+                break;
+
+            case 'LEOPARDO':
+                this.especie = 'LEOPARDO';
+                this.tamanho = 2;
+                this.biomas = ['savana'];
+                this.carnivoro = true;
+                break;
+
+            case 'CROCODILO':
+                this.especie = 'CROCODILO';
+                this.tamanho = 3;
+                this.biomas = ['rio'];
+                this.carnivoro = true;
+                break;
+            
+            case 'MACACO':
+                this.especie = 'MACACO';
+                this.tamanho = 1;
+                this.biomas = ['savana', 'floresta'];
+                this.carnivoro = false;
+                break;
+
+            case 'GAZELA':
+                this.especie = 'GAZELA';
+                this.tamanho = 2;
+                this.biomas = ['savana'];
+                this.carnivoro = false;
+                break;
+
+            case 'HIPOPOTAMO':
+                this.especie = 'HIPOPOTAMO';
+                this.tamanho = 4;
+                this.biomas = ['savana', 'rio'];
+                this.carnivoro = false;
+                break;
+        }
     }
 }
 
